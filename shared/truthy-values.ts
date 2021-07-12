4e412c5b8be748ff31bcc363fca92a7b6e614427
Francis Masha <francismasha96@gmail.com>
2021-07-12 15:35:26 +0300

    feat: add base controller (#4)

--- shared/truthy-values.ts
+++
@@ -1,8 +0,0 @@
-const getTruthyValuesFromObject = (object: Object): string[] => {
-  if (!object) return [];
-  try {
-    return Object.keys(object).filter(key => object[key] === true);
-  } catch (err) {
-    return [];
-  }
-}
